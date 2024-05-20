a7bab3d113c6ac0efc707f77ccb37db8860204a6
Nitzan <nitzantomer1998@gmail.com>
2024-05-20 18:32:25 +0300

    Update Query

--- src/queries/addonInstallQuery.js
+++ src/queries/addonInstallQuery.js
@@ -1,5 +1,5 @@
 const addonInstallQuery = {
-    insertInstall: 'INSERT INTO addon_installs DEFAULT VALUES',
+    insertInstall: 'INSERT INTO addon_installs (userConfig) VALUES ($1)',
     countInstalls: 'SELECT COUNT(*) FROM addon_installs'
 };
 
