efe749c717a36c3fd4ddbc18bc822d2a273fd502
Nitzan <nitzantomer1998@gmail.com>
2024-05-20 17:58:32 +0300

    Updating for UserConfig

--- src/utils/dataExtractor.js
+++ src/utils/dataExtractor.js
@@ -1,17 +1,19 @@
 import extractCompoundID from "./compoundIdExtractor.js";
 import extractFilename from "./filenameExtractor.js";
+import extractUserConfig from "./userConfigExtractor.js";
 import dataValidation from "../validations/dataValidation.js";
 
 
 const extractData = (params) => {
-    const { contentType, compoundID, extraArgs } = params;
+    const { userConfig: encodedUserConfig, contentType, compoundID, extraArgs } = params;
     const [imdbID, season, episode] = extractCompoundID(compoundID);
     const filename = extractFilename(extraArgs);
+    const userConfig = extractUserConfig(encodedUserConfig);
 
     const valid = dataValidation(contentType, imdbID, season, episode);
     if (valid === false) throw new Error(`Invalid Data: ContentType=${contentType}, imdbID=${imdbID}, season=${season}, episode=${episode}`);
 
-    return { imdbID, season, episode, filename };
+    return { userConfig, imdbID, season, episode, filename };
 };
 
 
