cd0359b2236cbf2926957b0e19bc9b380efd3a0b
Nitzan <nitzantomer1998@gmail.com>
2024-05-20 18:32:43 +0300

    Updating Query Param

--- src/services/dbService.js
+++ src/services/dbService.js
@@ -4,7 +4,7 @@ import downloadedContentQuery from "../queries/downloadedContentQuery.js";
 import watchedContentQuery from "../queries/watchedContentQuery.js";
 
 
-const insertAddonInstall = async () => { db.query(addonInstallQuery.insertInstall); };
+const insertAddonInstall = async (userConfig) => { db.query(addonInstallQuery.insertInstall, userConfig); };
 const insertDownloadedContent = async (provider, imdbID, season, episode) => { db.query(downloadedContentQuery.insertDownload, [provider, imdbID, season, episode]); };
 const insertWatchedContent = async (imdbID, season, episode) => { db.query(watchedContentQuery.insertWatch, [imdbID, season, episode]); };
 
