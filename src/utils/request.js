63548c0e7a527c16dc6f700710dd4c31ccca1568
Nitzan <nitzantomer1998@gmail.com>
2024-05-21 16:51:59 +0300

    Final Implementation

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,33 +1,15 @@
 import undici from "undici";
-import superagent from "superagent";
-import osConfig from "../configs/osConfig.js";
 
 
-const get = async (url, headers = {}) => await undici.request(url, { headers });
-const getBuffer = async (url, headers = {}) => await undici.request(url, { headers });
-const post = async (url, headers = {}, body = {}) => await undici.request(url, { headers, body: Buffer.from(JSON.stringify(body)) });
+const get = async (url, headers = {}) => await undici.request(url, { headers, maxRedirections: 10 });
+const getBuffer = async (url, headers = {}) => await undici.request(url, { headers, maxRedirections: 10 });
+const post = async (url, headers = {}, body = {}) => await undici.request(url, { method: "POST", headers, body: Buffer.from(JSON.stringify(body)), maxRedirections: 10 });
 
-
-const getTemp = async (url) => await superagent.get(url).set(osConfig.getHeaders());
-const postTemp = async (url, body, tries = 0) => {
-    return await superagent.post(url).set(osConfig.postHeaders()).send(body).catch((error) => {
-        if (tries > osConfig.getApiKeysLength() - 1) {
-            throw new Error("OpenSubtitles API Keys Are Maxed Out");
-        }
-        if (error.message === "Not Acceptable") {
-            osConfig.updateApiKey();
-            return postTemp(url, body, tries + 1);
-        }
-    });
-};
 
 const request = {
     get,
     getBuffer,
     post,
-    
-    getTemp,
-    postTemp
 }
 
 
