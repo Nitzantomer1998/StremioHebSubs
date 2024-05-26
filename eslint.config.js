210b12cc35703773a86a8e1a4f501f2dbc2d572f
Nitzan <nitzantomer1998@gmail.com>
2024-05-26 10:41:07 +0300

    Update Code Style Restrictions

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+const eslint = {
+    files: ["**/**/*.js"],
+    rules: {
+        "no-console": "error",
+        "no-unused-vars": "error",
+        "no-var": "error",
+        "prefer-const": "error",
+        "eqeqeq": ["error", "always"],
+        "func-style": ["error", "expression"],
+
+        "no-multiple-empty-lines": ["warn", { max: 2 }],
+        "consistent-return": "warn",
+
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "comma-dangle": ["error", "always-multiline"],
+        "indent": ["error", 4],
+        "prefer-arrow-callback": ["error", { allowNamedFunctions: false }],
+        "space-before-blocks": ["error", "always"],
+        "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+        "arrow-spacing": ["error", { before: true, after: true }],
+        "prefer-destructuring": ["warn", { object: true, array: true }],
+    },
+};
 
 
-export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-];
+export default eslint;
