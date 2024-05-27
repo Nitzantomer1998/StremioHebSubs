0866e818af65f53ec1d8b9a9497281c36ad4fa12
Nitzan <nitzantomer1998@gmail.com>
2024-05-27 08:13:18 +0300

    Uncomment db queries

--- src/controllers/stremioController.js
+++ src/controllers/stremioController.js
@@ -12,7 +12,7 @@ const getManifest = async (req, res) => {
     const { userConfig = "Default" } = req.params;
 
     loggerService.logInstall(userConfig);
-    // dbService.insertAddonInstall(userConfig);
+    dbService.insertAddonInstall(userConfig);
 
     res.send(manifestConfig);
 };
@@ -21,7 +21,7 @@ const getSubtitlesList = async (req, res) => {
     const { userConfig, imdbID, season, episode, filename } = extractData(req.params);
 
     loggerService.logWatch(imdbID, season, episode);
-    // dbService.insertWatchedContent(imdbID, season, episode);
+    dbService.insertWatchedContent(imdbID, season, episode);
 
     const stremioSubtitles = await stremioService.getSubtitlesList(userConfig, imdbID, season, episode);
     const sortedSubtitles = stremioService.sortSubtitlesByFilename(stremioSubtitles, filename);
@@ -33,7 +33,7 @@ const getSubtitleContent = async (req, res) => {
     const { provider, imdbID, season, episode, subtitleID } = req.params;
 
     loggerService.logDownload(provider, subtitleID);
-    // dbService.insertDownloadedContent(provider, imdbID, season, episode);
+    dbService.insertDownloadedContent(provider, imdbID, season, episode);
 
     const subtitleContent = await stremioService.getSubtitleContent(provider, subtitleID);
 
