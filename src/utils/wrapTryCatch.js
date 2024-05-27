75e6776abf923ac9f3d9a0386beee64a8db86dfb
Nitzan <nitzantomer1998@gmail.com>
2024-05-27 08:10:09 +0300

    Updating Structure

--- src/utils/wrapTryCatch.js
+++ src/utils/wrapTryCatch.js
@@ -3,11 +3,7 @@ import loggerService from "../services/loggerService.js";
 
 const wrapTryCatch = (fn) => async (req, res) => {
     try { await fn(req, res); }
-    catch (error) {
-        if (error.message.includes("Unexpected token 'e'")) loggerService.logError(error);
-        else loggerService.logError(error.message);
-        res.send({ subtitles: [] });
-    }
+    catch (error) { loggerService.logError(error.message); res.send({ subtitles: [] }); }
 };
 
 
