77eb9101eb339987a47e94677ad702aec7c16774
Nitzan <nitzantomer1998@gmail.com>
2024-06-02 17:58:47 +0300

    Setting Ktuvit Configuration

---
+++ src/configs/ktuvitConfig.js
@@ -0,0 +1,7 @@
+import ktuvitApi from "ktuvit-api";
+
+
+const ktuvitConfig = new ktuvitApi(await ktuvitApi.getLoginCookie("darksubsil1@gmail.com", "ZkCyMZfsIHt9HQK4eL8bbfaxXoNBjmFO9w39kt/gA14="));
+
+
+export default ktuvitConfig;
