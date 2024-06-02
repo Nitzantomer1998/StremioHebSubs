6491ba0aee01b678ac98c2036a64e3f2e43c4053
Nitzan <nitzantomer1998@gmail.com>
2024-06-02 14:08:22 +0300

    Update Watch Logger

--- src/services/loggerService.js
+++ src/services/loggerService.js
@@ -6,7 +6,7 @@ const logInstall = (userConfig) => { logger.info(["Install", `Addon Installed, u
 const logExpress = () => { logger.info(["Express", "Express Connected"]); };
 const logDatabase = () => { logger.info(["Database", "Database Connected"]); };
 const logDownload = (provider, subtitleID) => { logger.info(["Download", `provider=${provider}, subtitleID=${subtitleID}`]); };
-const logWatch = (imdbID, season, episode) => { logger.info(["Watch", `imdbID=${imdbID}, season=${season}, episode=${episode}`]); };
+const logWatch = (imdbID, season, episode, filename) => { logger.info(["Watch", `imdbID=${imdbID}, season=${season}, episode=${episode}, filename=${filename}`]); };
 
 const loggerService = {
     logError,
