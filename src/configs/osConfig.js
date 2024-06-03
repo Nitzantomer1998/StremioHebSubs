03faf7ba3b8beb0412ea96a81ebbd5ce1cbb047f
Nitzan <nitzantomer1998@gmail.com>
2024-06-03 19:31:31 +0300

    Updating Structure

--- src/configs/osConfig.js
+++ src/configs/osConfig.js
@@ -8,19 +8,8 @@ const apiKeys = process.env.OS_API_KEY.split(",");
 const getApiKeysLength = () => apiKeys.length;
 const getApiKey = () => apiKeys[apiKeyIndex];
 const updateApiKey = () => { apiKeyIndex = (apiKeyIndex + 1) % apiKeys.length; };
-
-const getHeaders = () => ({
-    "Content-Type": "application/json",
-    "Api-Key": getApiKey(),
-    "User-Agent": "StremioHebSub v1.0.0",
-});
-
-const postHeaders = () => ({
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Api-Key": getApiKey(),
-    "User-Agent": "StremioHebSub v1.0.0",
-});
+const getHeaders = () => ({ "Content-Type": "application/json", "Api-Key": getApiKey(), "User-Agent": "StremioHebSub v1.0.0" });
+const postHeaders = () => ({ "Accept": "application/json", "Content-Type": "application/json", "Api-Key": getApiKey(), "User-Agent": "StremioHebSub v1.0.0" });
 
 const osConfig = {
     getApiKeysLength,
