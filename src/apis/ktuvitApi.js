e755b8c3c633fabfa879fd8d8f8394005f7c6709
Nitzan <nitzantomer1998@gmail.com>
2024-06-04 12:39:36 +0300

    Setting Download Urls

--- src/apis/ktuvitApi.js
+++ src/apis/ktuvitApi.js
@@ -10,6 +10,7 @@ const ktuvitApi = {
     MOVIES_URL: process.env.KTUVIT_MOVIES_URL,
 
     DOWNLOAD_URL: process.env.KTUVIT_DOWNLOAD_URL,
+    DOWNLOAD_IDENTIFIER_URL: process.env.KTUVIT_DOWNLOAD_IDENTIFIER_URL,
 };
 
 
