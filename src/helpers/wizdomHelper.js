5c23d16923d3d4b229f064a0bc7f39e4e2da1dcc
Nitzan <nitzantomer1998@gmail.com>
2024-06-05 17:43:28 +0300

    Improving Error Tolerance from Wizdom

---
+++ src/helpers/wizdomHelper.js
@@ -0,0 +1,26 @@
+import request from "../utils/request.js";
+
+
+const safeFetchSubtitles = async (url, tries = 2) => {
+    let response;
+
+    while (tries) {
+        response = await request.get(url);
+
+        if (response.statusCode === 200) break;
+
+        tries--;
+    }
+
+    if (response.statusCode !== 200) throw new Error(`Wizdom SafeFetchSubtitles - Code=${response.statusCode}, Message=${response.body}`);
+    return response;
+};
+
+
+
+const wizdomHelper = {
+    safeFetchSubtitles,
+};
+
+
+export default wizdomHelper;
