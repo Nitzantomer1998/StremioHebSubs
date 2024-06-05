86aa57a33c2d961abd5fccc437bc57ddc2b5994b
Nitzan <nitzantomer1998@gmail.com>
2024-06-05 11:26:23 +0300

    Updating Page Link

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,5 @@
 const redirectToRegularInstall = () => window.open(`stremio://${window.location.host}/manifest.json`, "_blank");
-const redirectToConfigInstall = () => window.open("https://ec2-18-196-63-110.eu-central-1.compute.amazonaws.com/configure", "_blank");
+const redirectToConfigInstall = () => window.open("https://stremio-heb-subs.onrender.com/configure", "_blank");
 
 const redirectToDailyMetrics = () => window.open("https://nitzantomer1998.grafana.net/public-dashboards/96c725c12b5e49e7be756bd6b04a4b0e", "_blank");
 const redirectToMonthlyMetrics = () => window.open("https://nitzantomer1998.grafana.net/public-dashboards/181287d250ad46c4a3b879a67f7773c9", "_blank");
