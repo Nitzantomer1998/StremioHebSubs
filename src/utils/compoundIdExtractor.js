a661ecf43595a2462f4422c1276a53880d0a7e87
Nitzan <nitzantomer1998@gmail.com>
2024-06-07 19:31:19 +0300

    Removing Unneeded FIles

--- src/utils/compoundIdExtractor.js
+++
@@ -1,8 +0,0 @@
-const extractCompoundID = (compoundID) => {
-    const [id, season = "0", episode = "0"] = compoundID.split(":");
-
-    return [id, season, episode];
-};
-
-
-export default extractCompoundID;
--- src/utils/userConfigExtractor.js
+++
@@ -1,4 +0,0 @@
-const extractUserConfig = (userConfig) => userConfig === undefined ? ["Wizdom"] : JSON.parse(atob(userConfig));
-
-
-export default extractUserConfig;
