c5cc61df9c49c31ff73a0f488a44f71ed4180d3d
Nitzan <nitzantomer1998@gmail.com>
2024-06-13 17:03:58 +0300

    Removing Unused Queries

--- src/queries/addonInstallQuery.js
+++ src/queries/addonInstallQuery.js
@@ -1,6 +1,5 @@
 const addonInstallQuery = {
     insertInstall: "INSERT INTO addon_installs (userConfig) VALUES ($1)",
-    countInstalls: "SELECT COUNT(*) FROM addon_installs",
 };
 
 
--- src/queries/watchedContentQuery.js
+++ src/queries/watchedContentQuery.js
@@ -1,6 +1,5 @@
 const watchedContentQuery = {
     insertWatch: "INSERT INTO watched_content (imdb_id, season, episode) VALUES ($1, $2, $3)",
-    countWatches: "SELECT COUNT(*) FROM watched_content",
 };
 
 
