18529d1d2d0a2dc8874920a92fc325bf07faa44a
Nitzan <nitzantomer1998@gmail.com>
2024-06-17 08:29:25 +0300

    Modulize Subtitle Decode & Converter Files

--- src/services/subtitleService.js
+++ src/services/subtitleService.js
@@ -1,71 +1,44 @@
-import loggerService from "../services/loggerService.js";
+import chardet from "chardet";
+import iconv from "iconv-lite";
+
+import subtitleConfig from "../configs/subtitleConfig.js";
 
 
-const stremioSubtitleFormats = ["srt", "vtt"];
-const customSubtitleFormats = ["ass"];
-const ignoredSubtitleFormats = ["sub"];
+const convertSubtitle = (subtitleContent) => {
+    const subtitleFormat = detectSubtitleFormat(subtitleContent);
 
+    if (subtitleConfig.supportedSubtitleFormats.includes(subtitleFormat)) return content;
+    if (subtitleConfig.customSubtitleFormats.includes(subtitleFormat)) return subtitleConfig.subtitleConverter[subtitleFormat](subtitleContent);
+    if (subtitleConfig.unsupportedSubtitleFormats.includes(subtitleFormat)) return "";
 
-const subtitleFormatRegex = {
-    srt: /^(\d+)(\s*)(\d{2}:\d{2}:\d{2},\d{3})(\s*-->\s*)(\d{2}:\d{2}:\d{2},\d{3})/, // Regex for perfect SRT (there will be valid srt that are not perfect, i will catch them and improve ASAP) 
-    vtt: /^(\d+)(\s*)(\d{2}:\d{2}:\d{2}\.\d{3})(\s*-->\s*)(\d{2}:\d{2}:\d{2}\.\d{3})/, // Regex for perfect VTT (there will be valid srt that are not perfect, i will catch them and improve ASAP)
-    ass: /^[\s\r\n]*\[Script Info\]\r?\n.*[\s\r\n]*\[Events\]\r?\n/g,
-    sub: /^\{\d+(.\d+)?\}\{\d+(.\d+)?\}(.*)/,
+    loggerService.logError(`Unsupported Subtitle Format: ${subtitleFormat}`);
+    return "";
 };
 
 const detectSubtitleFormat = (subtitleContent) => {
     subtitleContent = subtitleContent.trim();
 
-    if (subtitleFormatRegex.sub.test(subtitleContent)) return "sub";
-    if (subtitleFormatRegex.vtt.test(subtitleContent)) return "vtt";
-    if (subtitleFormatRegex.ass.test(subtitleContent)) return "ass";
-    if (subtitleFormatRegex.srt.test(subtitleContent)) return "srt";
+    if (subtitleConfig.subtitleFormatsRegex.srt.test(subtitleContent)) return "srt";
+    if (subtitleConfig.subtitleFormatsRegex.vtt.test(subtitleContent)) return "vtt";
+    if (subtitleConfig.subtitleFormatsRegex.ass.test(subtitleContent)) return "ass";
+    if (subtitleConfig.subtitleFormatsRegex.sub.test(subtitleContent)) return "sub";
 
     return "unknown";
 };
 
-const convertSubtitleContent = (subtitleFormat, subtitleContent) => {
-    const converters = {
-        ass: convertAssToSrt,
-    };
+const decodeSubtitle = async (subtitleContent) => {
+    const bufferArray = Buffer.from(subtitleContent);
+    const detectedEncoding = chardet.detect(bufferArray);
+    const decodeSubtitleContent = iconv.decode(bufferArray, detectedEncoding);
 
-    return converters[subtitleFormat] ? converters[subtitleFormat](subtitleContent) : "";
+    return decodeSubtitleContent;
 };
 
-const convertAssToSrt = (subtitleContent) => {
-    let srtContent = "";
-    let eventCount = 0;
-
-    const lines = subtitleContent.split("\n");
-    const dialogueRegex = /Dialogue: \d+,(\d+:\d+:\d+\.\d+),(\d+:\d+:\d+\.\d+),.*,,\d+,\d+,\d+,,(.*)/;
-
-    lines.forEach(line => {
-        const match = dialogueRegex.exec(line);
-
-        if (match) {
-            eventCount++;
-
-            const start = match[1].replace(".", ",");
-            const end = match[2].replace(".", ",");
-            const text = match[3].replace(/\\N/g, "\n");
-
-            srtContent += `${eventCount}\n${start} --> ${end}\n${text}\n\n`;
-        }
-    });
-
-    return srtContent.trim();
+const subtitleService = {
+    convertSubtitle,
+    detectSubtitleFormat,
+    decodeSubtitle,
 };
 
 
-const convertSubtitle = (subtitleContent) => {
-    const detectedFormat = detectSubtitleFormat(subtitleContent);
-
-    if (stremioSubtitleFormats.includes(detectedFormat)) return subtitleContent;
-    if (customSubtitleFormats.includes(detectedFormat)) return convertSubtitleContent(detectedFormat, subtitleContent);
-    if (ignoredSubtitleFormats.includes(detectedFormat)) return "";
-
-    loggerService.logError(`Unsupported Subtitle Format: ${detectedFormat}`);
-    return "";
-};
-
-export default convertSubtitle;
+export default subtitleService;
