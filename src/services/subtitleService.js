6619431036feb64c92c2c819043e6de1b4a0578a
Nitzan <nitzantomer1998@gmail.com>
2024-06-23 18:32:13 +0300

    Adding Subtitle Functionalities & Pipeline

--- src/services/subtitleService.js
+++ src/services/subtitleService.js
@@ -7,8 +7,24 @@ import divideSubtitle from "../utils/divideSubtitle.js";
 import googleApi from "../apis/googleApi.js";
 
 
-const cleanSubtitle = (subtitleContent) => subtitleContent.replace(/<[^>]*>/g, "");
+const subtitlePipeline = async (subtitleContent, isTranslated = true) => {
+    const cleanedSubtitle = cleanSubtitle(subtitleContent);
+    const decodedSubtitle = await decodeSubtitle(cleanedSubtitle);
+    const detectedSubtitleFormat = detectSubtitleFormat(decodedSubtitle);
+    const convertedSubtitle = convertSubtitle(decodedSubtitle, detectedSubtitleFormat);
+    const translatedSubtitle = isTranslated ? convertedSubtitle : await translateSubtitle(convertedSubtitle);
+    const fixedSubtitle = fixSubtitlePunctuation(translatedSubtitle);
 
+    return fixedSubtitle;
+};
+const cleanSubtitle = (subtitleContent) => subtitleContent.replace(/<[^>]*>/g, "");
+const decodeSubtitle = async (subtitleContent) => {
+    const bufferArray = Buffer.from(subtitleContent);
+    const detectedEncoding = chardet.detect(bufferArray);
+    const decodeSubtitleContent = iconv.decode(bufferArray, detectedEncoding);
+
+    return decodeSubtitleContent;
+};
 const detectSubtitleFormat = (subtitleContent) => {
     subtitleContent = subtitleContent.trim();
 
@@ -19,15 +35,12 @@ const detectSubtitleFormat = (subtitleContent) => {
 
     return "unknown";
 };
-
-const decodeSubtitle = async (subtitleContent) => {
-    const bufferArray = Buffer.from(subtitleContent);
-    const detectedEncoding = chardet.detect(bufferArray);
-    const decodeSubtitleContent = iconv.decode(bufferArray, detectedEncoding);
-
-    return decodeSubtitleContent;
+const convertSubtitle = (subtitleContent, subtitleFormat) => {
+    if (subtitleConfig.supportedSubtitleFormats.includes(subtitleFormat)) return subtitleContent;
+    else if (subtitleConfig.unsupportedSubtitleFormats.includes(subtitleFormat)) return subtitleConfig.defaultSubtitleContent;
+    else if (subtitleConfig.customSubtitleFormats.includes(subtitleFormat)) return subtitleConfig.subtitleConverter[subtitleFormat](subtitleContent);
+    else throw new Error(`Unsupported Subtitle Format: ${subtitleFormat}`);
 };
-
 const translateSubtitle = async (subtitleContent) => {
     const chunks = divideSubtitle(subtitleContent);
     const translationPromises = [];
@@ -36,7 +49,7 @@ const translateSubtitle = async (subtitleContent) => {
         const url = `${googleApi.TRANSLATE_URL}q=${encodeURIComponent(chunk)}`;
         const promise = httpService.safeGetRequest(url)
             .then(response => response.body.json())
-            .then(body => body && body[0] && body[0][0] && body[0].map(s => s[0]).join(""));
+            .then(body => body[0].map(s => s[0]).join(""));
 
         translationPromises.push(promise);
     }
@@ -44,22 +57,29 @@ const translateSubtitle = async (subtitleContent) => {
 
     return translations.join(" ");
 };
+const fixSubtitlePunctuation = (subtitleContent) => {
+    const punctuationMarks = ["...", "..", ".", ",", "?", "!", ":"];
+    const modifiedSubtitleLines = [];
 
-const convertSubtitle = (subtitleContent) => {
-    const subtitleFormat = detectSubtitleFormat(subtitleContent);
+    const subtitleLines = subtitleContent.split("\n");
+    for (let subtitleLine of subtitleLines) {
+        subtitleLine = subtitleLine.replace(/-/g, "");
 
-    if (subtitleConfig.supportedSubtitleFormats.includes(subtitleFormat)) return subtitleContent;
-    else if (subtitleConfig.unsupportedSubtitleFormats.includes(subtitleFormat)) return subtitleConfig.defaultSubtitleContent;
-    else if (subtitleConfig.customSubtitleFormats.includes(subtitleFormat)) return subtitleConfig.subtitleConverter[subtitleFormat](subtitleContent);
-    else throw new Error(`Unsupported Subtitle Format: ${subtitleFormat}`);
+        for (const mark of punctuationMarks) {
+            if (subtitleLine.endsWith(mark)) {
+                subtitleLine = mark + subtitleLine.slice(0, -mark.length);
+                break;
+            }
+        }
+
+        modifiedSubtitleLines.push(subtitleLine);
+    }
+
+    return modifiedSubtitleLines.join("\n");
 };
 
 const subtitleService = {
-    cleanSubtitle,
-    detectSubtitleFormat,
-    decodeSubtitle,
-    translateSubtitle,
-    convertSubtitle,
+    subtitlePipeline,
 };
 
 
