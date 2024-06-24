42395b97d737e607944c2772e42fb93fa287bc2d
Nitzan <nitzantomer1998@gmail.com>
2024-06-24 08:32:22 +0300

    Adding VTT Conversion

--- src/helpers/subtitleConverterHelper.js
+++ src/helpers/subtitleConverterHelper.js
@@ -1,29 +1,50 @@
-const convertAssToSrt = (content) => {
+const convertVTT = (subtitleContent) => {
     const srtLines = [];
     let eventCount = 0;
 
-    const lines = content.split("\n");
-    const dialoguePattern = /^Dialogue: \d+,(\d+:\d+:\d+\.\d+),(\d+:\d+:\d+\.\d+),.*,,\d+,\d+,\d+,,(.*)/;
+    const lines = subtitleContent.split("\n");
+    const dialoguePattern = /^(\d+:\d+:\d+\.\d+)\s*-->\s*(\d+:\d+:\d+\.\d+)\s*(.*)/;
 
-    lines.forEach(line => {
+    lines.forEach((line) => {
         const match = dialoguePattern.exec(line);
+        if (match === false) return;
 
-        if (match) {
-            eventCount++;
+        eventCount++;
+        const startTime = match[1].replace(".", ",");
+        const endTime = match[2].replace(".", ",");
+        const dialogueText = match[3];
 
-            const startTime = match[1].replace(".", ",");
-            const endTime = match[2].replace(".", ",");
-            const dialogueText = match[3].replace(/\\N/g, "\n");
+        srtLines.push(`${eventCount}\n${startTime} --> ${endTime}\n${dialogueText}\n`);
+    });
 
-            srtLines.push(`${eventCount}\n${startTime} --> ${endTime}\n${dialogueText}\n`);
-        }
+    return srtLines.join("\n").trim();
+};
+
+const convertASS = (subtitleContent) => {
+    const srtLines = [];
+    let eventCount = 0;
+
+    const lines = subtitleContent.split("\n");
+    const dialoguePattern = /^Dialogue:\s*\d+,(\d+:\d+:\d+\.\d+),(\d+:\d+:\d+\.\d+),.*,,(.*)/;
+
+    lines.forEach((line) => {
+        const match = dialoguePattern.exec(line);
+        if (match === false) return;
+
+        eventCount++;
+        const startTime = match[1].replace(".", ",");
+        const endTime = match[2].replace(".", ",");
+        const dialogueText = match[3].replace(/\\N/g, "\n");
+
+        srtLines.push(`${eventCount}\n${startTime} --> ${endTime}\n${dialogueText}\n`);
     });
 
     return srtLines.join("\n").trim();
 };
 
 const subtitleConverterHelper = {
-    convertAssToSrt,
+    convertVTT,
+    convertASS,
 };
 
 
